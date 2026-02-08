8e6e21a18feb2cdc65ef6214bfd34feb8a24263e
Zimeng Yang <zimengyang8@gmail.com>
2026-02-08 14:39:55 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Zimeng Yang on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BipTheGuy
+//
+//  Created by Zimeng Yang on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
